41ef1d7fbe12cde06c88ed8146d108adbd8f8fad
Nixential <nixential@protonmail.com>
2024-09-12 11:24:31 +0000

    add fib ex

--- function_ex/power.hs
+++ function_ex/power.hs
@@ -1,3 +1,11 @@
+-- power :: Int -> Int -> Int
+-- power _ 0 = 1
+-- power n x = n * power n (x - 1)
+
 power :: Int -> Int -> Int
-power _ 0 = 1
-power n x = n * power n (x - 1)
+power x 0 = 1
+power x p
+	| even p = n * n
+	| otherwise = n * n * x
+	where
+		n = power x (p `div` 2)
